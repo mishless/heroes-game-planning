d6814006f9dd0d441c0c3d14598455538015cde8
Angel Paredes Barato <angel.barato@izettle.com>
2018-10-09 12:54:39 +0200

    Add Babel to have ES6 features.
    Add generated files to gitignore
    
    Run no-vars jscodemod and templates literal codemod

--- processor.js
+++ processor.js
@@ -1,33 +1,35 @@
-var strips = require('strips');
-var mapGenerator = require('./map_generator')
-var fs = require('fs');
+const strips = require('strips');
+const mapGenerator = require('./map_generator');
+const fs = require('fs');
 // Load the domain and problem.
 
-problem = mapGenerator.generate(5, 6, 9, [1, 2]);
-fs.writeFile("./hero_problem.pddl", problem, function(err) {
+const a = [1, 2, 3, 4]
+const [b, c] = a;
+let problem = mapGenerator.generate(5, 6, 9, [1, 2]);
+fs.writeFile("./hero_problem.pddl", problem, err => {
     if(err) {
         return console.log(err);
     }
     console.log("The file was saved!");
 });
 
-strips.load('./hero_domain_final.pddl', './hero_problem.pddl', function(domain, problem) {
+strips.load('./hero_domain_final.pddl', './hero_problem.pddl', (domain, problem) => {
     // Run the problem against the domain, using depth-first-search.
-    var solutions = strips.solve(domain, problem, cost);
+    const solutions = strips.solve(domain, problem, cost);
     // Display each solution.
-    for (var i in solutions) {
-        var solution = solutions[i];
-        console.log('- Solution found in ' + solution.steps + ' steps!');
-        for (var i = 0; i < solution.path.length; i++) {
-            console.log((i + 1) + '. ' + solution.path[i]);
+    for (let i in solutions) {
+        const solution = solutions[i];
+        console.log(`- Solution found in ${solution.steps} steps!`);
+        for (let j = 0; j < solution.path.length; j++) {
+            console.log(`${j + 1}. ${solution.path[j]}`);
         }
     }
 });
 
 function cost(state) {
-  var cost = 100;
-  for (var i in state.actions) {
-    var action = state.actions[i].action;
+  let cost = 100;
+  for (let i in state.actions) {
+    const action = state.actions[i].action;
 
     if (action === "has-castle") {
       cost -= 20;

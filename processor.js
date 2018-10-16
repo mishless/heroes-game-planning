cab4ebdd77b32460e89d43c2032622c062fae30c
Mihaela Stoycheva <mihaela.stoycheva@gmail.com>
2018-10-16 18:01:54 +0200

    Add results and new processor

--- processor.js
+++ processor.js
@@ -22,70 +22,70 @@ fs.writeFile("./hero_problem.pddl" , PROBLEM, err => {
 let results = {};
 
 const runLoader = async () => {
-for (let p=0; p<5; p++) {
+for (let p=0; p<3; p++) {
   results[p] = {};
   await new Promise(resolve => strips.load(
-    "./Maps/hero_domain_3_by_3_1_monster.pddl",
-    "./Maps/hero_problem_3_by_3_1_block_1_monster_"+p+".pddl",
+    "./Maps/hero_domain_3_by_3_2_monsters.pddl",
+    "./Maps/hero_problem_3_by_3_1_block_2_monsters_"+p+".pddl",
     (domain, problem) => {
       // Get encoding for GA
       var mapping = GA.encode(domain, problem);
-      // Generate initial population
-      var initialPopulation = GA.generateIntialPopulation(
-        domain,
-        problem,
-        strips.applicableActions,
-        mapping,
-        config.chromosome_size,
-        config.population_size
-      );
-      if (initialPopulation.length < 0) {
-        console.log("Invalid grid generated - no valid first move.");
-      }
       let times = [];
       let lengths = [];
       for (let j = 0; j<5; j++) {
+        results[p][j] = {};
+        // Generate initial population
+        var initialPopulation = GA.generateIntialPopulation(
+          domain,
+          problem,
+          strips.applicableActions,
+          mapping,
+          config.chromosome_size,
+          config.population_size
+        );
+        if (initialPopulation.length < 0) {
+          console.log("Invalid grid generated - no valid first move.");
+        }
         var start = new Date().getTime();
         for (let i=0; i<config.generations; i++) {
           initialPopulation = GA.generateNewPopulation(initialPopulation, domain, mapping, problem.states[0], problem.states[1]);
           let test = GA.getTheFittest(initialPopulation, domain, mapping, problem.states[0], problem.states[1]);
-          //console.log("----------------------------------------");
-          //console.log(test.individual);
-          //GA.printFitness(test.individual, domain, mapping, problem.states[0], problem.states[1]);
           let collisionsAtEndObject = fitnessFunction.getCountCollisionsAtTheEnd(domain, mapping, test.individual, problem.states[0], problem.states[1]);
           let collisionsAtEnd = collisionsAtEndObject.goalPreconditions;
           let length = collisionsAtEndObject.length;
-          //console.log(collisionsAtEnd);
-          if (collisionsAtEnd === 1 && results[length] === undefined) {
-              var end = new Date().getTime();
-              results[p][length + 1] = {"length": length + 1, "iteration": i, "time": end - start, "path": test.individual, "fitness": test.bestFitness};
+          if (collisionsAtEnd === 1 && results[p][j][length + 1] === undefined) {
+              let end = new Date().getTime();
+              results[p][j][length + 1] = {"length": length + 1, "iteration": i, "time": end - start, "path": test.individual, "fitness": test.bestFitness};
           }
-          //console.log(test.bestFitness);
-          //console.log("----------------------------------------");
           if (i % 10 === 0) {
             GA.cleanLoops(initialPopulation);
           }
-          var end = new Date().getTime();
-          //console.log("Time (ms): "+(end - start));
+          //console.log("----------------------------------------");
+          //console.log(test.individual);
+          //GA.printFitness(test.individual, domain, mapping, problem.states[0], problem.states[1]);
+          //console.log(test.bestFitness);
+          //console.log("----------------------------------------");
         }
-        times.push(end);
+        let endOfIteration = new Date().getTime();
+        times.push(endOfIteration - start);
         let bestPath = Infinity;
-        for (let result in results[p]) {
-          if (results[p][result].length < bestPath) {
-            bestPath = results[p][result].length;
+        for (let result in results[p][j]) {
+          if (results[p][j][result].length < bestPath) {
+            bestPath = results[p][j][result].length;
           }
         }
-        lengths.push(bestPath);
-      }
-      console.log("For map 3 by 3 with 1 monster # " + p + ": Average length of " + average(lengths) + " and average time of " + average(times));
-      if (p === 4) {
-        for (let i = 0; i<5; i++) {
-          console.log("Map # " + i + ": " + JSON.stringify(results[i]));
+        if (bestPath != Infinity) {
+          lengths.push(bestPath);
         }
       }
-        resolve()
+      console.log("For map 3 by 3 with 1 monster # " + p + ": Average length of " + average(lengths) + " and average time of " + average(times));
+      resolve();
     }))
+  }
+  for (let i = 0; i<3; i++) {
+    for (let j = 0; j<5; j++) {
+      console.log("Map # " + i + ", iteration # " + j + ": " + JSON.stringify(results[i][j]));
+    }
+  }
 }
-}
-
 runLoader();
